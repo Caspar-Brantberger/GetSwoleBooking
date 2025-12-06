bd086a66dac24640b92ac051d947f209f4d72f14
Caspar Brantberger <brantbergercaspar@gmail.com>
2025-12-06 23:37:40 +0100

    added so I can have empty files

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="min-h-screen bg-red-500 flex items-center justify-center">
       <button className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-        Klicka här
+        Log in
       </button>
     </div>
   );
