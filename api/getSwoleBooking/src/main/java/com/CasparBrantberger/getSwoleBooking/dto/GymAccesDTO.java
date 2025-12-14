94a7ea6944ac14b3bbc77bf9d6eda83e495dea6c
Caspar Brantberger <brantbergercaspar@gmail.com>
2025-12-15 00:12:43 +0100

    added DTO classes

--- api/getSwoleBooking/src/main/java/com/CasparBrantberger/getSwoleBooking/dto/GymAccesDTO.java
+++ api/getSwoleBooking/src/main/java/com/CasparBrantberger/getSwoleBooking/dto/GymAccesDTO.java
@@ -1,4 +1,23 @@
 package com.CasparBrantberger.getSwoleBooking.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GymAccesDTO {
+    private Long id;
+    private long userId;
+    private String username;
+    private Long accesOptionId;
+    private String accesOptionType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String status;
 }
