2e3d4fadfdd5e3a2f103526ef320dd9927b6fadb
Caspar Brantberger <brantbergercaspar@gmail.com>
2025-12-15 00:03:05 +0100

    added repository for both user and GymAccess

--- api/getSwoleBooking/src/main/java/com/CasparBrantberger/getSwoleBooking/repository/GymAccesRepository.java
+++ api/getSwoleBooking/src/main/java/com/CasparBrantberger/getSwoleBooking/repository/GymAccesRepository.java
@@ -1,4 +1,17 @@
 package com.CasparBrantberger.getSwoleBooking.repository;
 
-public interface GymAccesRepository {
+import com.CasparBrantberger.getSwoleBooking.model.AccesStatus;
+import com.CasparBrantberger.getSwoleBooking.model.GymAcces;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GymAccesRepository  extends JpaRepository<GymAcces, Long> {
+
+    List<GymAcces> findByUserId(Long userId);
+
+    List<GymAcces> findByAccesOptionId(Long accesOptionId);
+    List<GymAcces> findByStatus(AccesStatus status);
 }
